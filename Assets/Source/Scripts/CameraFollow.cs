acbf43360cf478a69b5dbd6e8c77caa17e516bfc
Pelip Alexandr <sasha.pelip@gmail.com>
2023-12-12 17:37:33 +0300

    Fix online behaviour

---
+++ Assets/Source/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+public class CameraFollow : NetworkBehaviour
+{
+    private Transform _camera;
+
+    private void Start()
+    {
+        if (isLocalPlayer == false)
+            return;
+        
+        _camera = Camera.main.transform;
+    }
+
+    private void LateUpdate()
+    {
+        if (isLocalPlayer == false)
+            return;
+
+        _camera.SetPositionAndRotation(transform.position, transform.rotation);
+    }
+}
---
+++ Assets/Source/Scripts/PlayerCanvas.cs
@@ -0,0 +1,13 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class PlayerCanvas : NetworkBehaviour
+{
+    [SerializeField] private GameObject _canvas;
+    
+    private void Start()
+    {
+        if (isLocalPlayer)
+            _canvas.SetActive(true);
+    }
+}
