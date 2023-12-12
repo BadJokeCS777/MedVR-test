eb5eba59314c1016d57e79aa8fa9de4d34275bc9
Pelip Alexandr <sasha.pelip@gmail.com>
2023-12-12 16:11:19 +0300

    Add spawning character

---
+++ Assets/Source/Scripts/Character.cs
@@ -0,0 +1,48 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class Character : NetworkBehaviour
+{
+    [SerializeField, Min(1f)] private float _lifetime;
+    [SerializeField] private Vector2 _speedRange;
+    [SerializeField] private Vector2 _moveDurationRange;
+
+    private float _lifeTimer;
+    private float _moveTimer;
+    private float _speed;
+    private float _duration;
+    
+    private void Start()
+    {
+        _lifeTimer = 0;
+        
+        UpdateValues();
+    }
+
+    private void Update()
+    {
+        float deltaTime = Time.deltaTime;
+
+        if (_lifeTimer > _lifetime)
+        {
+            NetworkServer.Destroy(gameObject);
+            return;
+        }
+        
+        if (_moveTimer >= _duration)
+            UpdateValues();
+
+        transform.Translate(_speed * deltaTime * transform.forward);
+        
+        _moveTimer += deltaTime;
+        _lifeTimer += deltaTime;
+    }
+
+    private void UpdateValues()
+    {
+        _moveTimer = 0f;
+        _speed = Random.Range(_speedRange.x, _speedRange.y);
+        _duration = Random.Range(_moveDurationRange.x, _moveDurationRange.y);
+        transform.forward = new Vector3(Random.Range(-1f, 1f),0f, Random.Range(-1f, 1f)).normalized;
+    }
+}
---
+++ Assets/Source/Scripts/CharacterSpawner.cs
@@ -0,0 +1,27 @@
+﻿using Mirror;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSpawner : NetworkBehaviour
+{
+    [SerializeField] private Character _characterPrefab;
+    [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private Button _spawnButton;
+
+    private void OnEnable()
+    {
+        _spawnButton.onClick.AddListener(CmdSpawn);
+    }
+
+    private void OnDisable()
+    {
+        _spawnButton.onClick.RemoveListener(CmdSpawn);
+    }
+
+    [Command]
+    private void CmdSpawn()
+    {
+        Character arrow = Instantiate(_characterPrefab, _spawnPoint.position, Quaternion.identity);
+        NetworkServer.Spawn(arrow.gameObject);
+    }
+}
